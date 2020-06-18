6daa1f0fbc46233c0ece75c44e83bc18f4e183bb
Julia Kravchenko <yuliaxaomi@gmail.com>
2020-06-18 13:34:20 +0300

    init commit

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,9 @@
+INSERT INTO hibernate_sequence
+VALUES (1);
+
+
+/*INSERT INTO user_role
+VALUES (24,'USER'),(27,'USER'),(28,'USER'),(30,'USER'),(33,'USER'),(36,'USER'),(37,'USER'),(38,'USER'),(39,'USER'),(43,'USER'),(44,'USER'),(46,'USER'),(48,'USER');
+
+INSERT INTO usr
+VALUES (24,true,'qwe','user5','user5','13.06.2020'),(27,true,'qwerty','qwe','qwe','13.06.2020'),(28,true,'abc','us','us','13.06.2020'),(30,true,'mail.ru','user1','user1','13.06.2020'),(33,true,'qwe@yandex.by','ser','ser','14.06.2020'),(36,true,'qwe','usr','usr','14.06.2020'),(37,true,'yulia@yandex.by','123','yulia','15.06.2020'),(38,true,'katya@yandex.by','kate','katya','15.06.2020'),(39,true,'myemail@gmail.com','12345','1234','16.06.2020'),(43,true,'pass@mail.ru','pass','user','16.06.2020'),(44,true,'user@mail.ru','ur','ur','16.06.2020'),(46,true,'masha@mail.ru','mashaa','masha','17.06.2020'),(48,true,'url@mail.ru','qwerty','qwerty','17.06.2020');*/
